344f5ac5c54c1c856e3382b1d302f5f065349b14
Abhishek Mishra <abmishra700@gmail.com>
2021-05-03 19:06:52 +0530

    netflix series

---
+++ src/Sdata.js
@@ -0,0 +1,22 @@
+const sdata = [
+  {
+    sname: "Dark",
+    imgsrc: "Dark  Netflix Official Site.jpg",
+    title: "NETFLIX SERIES",
+    links: "https://www.netflix.com/in/title/80100172",
+  },
+  {
+    sname: "Peaky Blinders",
+    imgsrc: "Peaky Blinders  Netflix Official Site.jpg",
+    title: "NETFLIX SERIES",
+    links: "https://www.netflix.com/in/title/80002479",
+  },
+  {
+    sname: "Breaking Bad",
+    imgsrc: "Breaking Bad  Netflix.jpg",
+    title: "NETFLIX SERIES",
+    links: "https://www.netflix.com/in/title/70143836",
+  },
+];
+
+export default sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card";
+import Sdata from "./Sdata";
+import "./index.css";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <>
+    <h1 className="heading_style">Netflix Top 3 series</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    {Sdata.map((val) => {
+      return (
+        <Card
+          sname={val.sname}
+          imgsrc={val.imgsrc}
+          title={val.title}
+          links={val.links}
+        />
+      );
+    })}
+  </>,
+  document.getElementById("root")
+);
