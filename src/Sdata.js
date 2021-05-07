fb2a197c181957749e35c71588e7de8d88504bb5
Abhishek Mishra <abmishra700@gmail.com>
2021-05-07 15:11:13 +0530

    add key value

--- src/Sdata.js
+++ src/Sdata.js
@@ -1,17 +1,20 @@
 const sdata = [
   {
+    id:1,
     sname: "Dark",
     imgsrc: "Dark  Netflix Official Site.jpg",
     title: "NETFLIX SERIES",
     links: "https://www.netflix.com/in/title/80100172",
   },
   {
+    id:2,
     sname: "Peaky Blinders",
     imgsrc: "Peaky Blinders  Netflix Official Site.jpg",
     title: "NETFLIX SERIES",
     links: "https://www.netflix.com/in/title/80002479",
   },
   {
+    id:3,
     sname: "Breaking Bad",
     imgsrc: "Breaking Bad  Netflix.jpg",
     title: "NETFLIX SERIES",
